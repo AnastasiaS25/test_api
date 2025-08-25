7d31c0a2a471686fc1355e7aa60a631b203e0fe5
AnastasiaS25 <anlsob@yandex.ru>
2025-08-25 14:00:33 +0300

    app.py

---
+++ ex_json.py
@@ -0,0 +1,20 @@
+from json import dumps, loads, dump, load
+
+json_string = '{"имя" : "John", "возраст" : 36, "kids" : ["Mike", "Anna"], "pets" : {"dog" : "Rex", "cat" : "Barsik"}, "wife" : {"name" : "Jane", "age" : 6}}' #json-строка
+
+print(json_string)
+
+json_obj = loads(json_string) #создаем объект из строки
+
+print(json_obj)
+
+json_string = dumps(json_obj, indent=4, ensure_ascii=False, separators = ("", '=')) #создаем строку из объекта
+
+print(json_string)
+
+with open("data.json", "w", encoding="utf-8") as file:
+    dump(json_obj, file, indent=4, ensure_ascii=False) #записываем объект в файл
+
+with open("data.json", "r", encoding="utf-8") as file:
+    json_obj = load(file) #читаем объект из файла
+    print(json_obj)    
